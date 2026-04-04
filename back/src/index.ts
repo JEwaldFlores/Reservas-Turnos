22b41480801b7d8391defc262676a9f09377403d
jannelyewald <jannelyewald@hotmail.com>
2026-04-03 18:08:12 -0600

    deploy2

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,8 +7,8 @@ import { AppDataSource } from "./config/data-source";
 AppDataSource.initialize()
     .then(res=>{
         console.log("Conexión a la base de datos exitosa");
-        server.listen(PORT, ()=> {
-         console.log(`Server listening on http://localhost:${PORT}`)
+        server.listen(PORT, "0.0.0.0", () => {
+          console.log(`Server listening on port ${PORT}`);
         })
     })
     .catch (error => console.log(error));
